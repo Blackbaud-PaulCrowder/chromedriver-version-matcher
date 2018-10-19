094f1685d0480381bf4c7af9ce780e50c98abfb7
Bobby Earl <bobby.earl@blackbaud.com>
2018-10-18 20:31:59 -0400

    Added support for win32 platform

--- index.js
+++ index.js
@@ -4,29 +4,50 @@ const chromeFinder = require('chrome-launcher/dist/src/chrome-finder');
 const chromeUtils = require('chrome-launcher/dist/src/utils');
 const parser = require('xml2js');
 
+function getMajorFromVersion(version) {
+  return +version.split('.')[0];
+}
+
 function getChromeMajorVersion() {
   const promise = new Promise((resolve, reject) => {
     const platform = chromeUtils.getPlatform();
-
     const installations = chromeFinder[platform]();
 
     if (installations && installations.length > 0) {
-      const source = spawn(
-        installations[0],
-        [
-          '--version'
-        ]
-      );
-    
-      source.stdout.on('data', (data) => {
-        const chromeVersion = data.toString().trim().substr('Google Chrome '.length);
-        const chromeMajorVersion = +chromeVersion.split('.')[0];
-  
-        resolve({
-          chromeVersion: chromeVersion,
-          chromeMajorVersion: chromeMajorVersion
+      if (platform === 'win32') {
+
+        const source = spawn('wmic');
+        source.stdin.end(
+          `datafile where "name='${installations[0].replace(/\\/g, '\\\\')}'" get version /value`
+        );
+
+        source.stderr.on('data', () => reject('Unable to get Chrome version from wmic'));
+        source.stdout.on('data', (data) => {
+          const dataCleaned = data.toString().toLowerCase().trim().split('\r')[0];
+          if (dataCleaned.indexOf('version=') > -1) {
+            const chromeVersion = dataCleaned.split('=')[1];
+            resolve({
+              chromeVersion: chromeVersion,
+              chromeMajorVersion: getMajorFromVersion(chromeVersion)
+            });
+          }
         });
-      });
+
+      } else {
+
+        const source = spawn(installations[0], ['--version']);
+        source.stderr.on('data', () => reject('Unable get Chrome version'));
+        source.stdout.on('data', (data) => {
+          const chromeVersion = data.toString()
+            .trim().substr('Google Chrome '.length).split(' ')[0];
+          resolve({
+            chromeVersion: chromeVersion,
+            chromeMajorVersion: getMajorFromVersion(chromeVersion)
+          });
+        });
+
+      }
+
     } else {
       reject('No local installation of Chrome was found.');
     }
@@ -38,7 +59,7 @@ function getChromeMajorVersion() {
 function getLatestChromeDriverVersion() {
   return new Promise((resolve, reject) => {
     request(
-      'https://chromedriver.storage.googleapis.com/', 
+      'https://chromedriver.storage.googleapis.com/',
       (error, response, body) => {
         if (error) {
           reject(error);
@@ -57,18 +78,18 @@ function getLatestChromeDriverVersion() {
                 .sort((a, b) => {
                   const aMinorVersion = +a.split('.')[1];
                   const bMinorVersion = +b.split('.')[1];
-    
+
                   if (aMinorVersion < bMinorVersion) {
                     return 1;
                   }
-    
+
                   if (bMinorVersion < aMinorVersion) {
                     return -1;
                   }
-    
+
                   return 0;
                 });
-                
+
               resolve(versionNumbers[0]);
             }
           });
@@ -81,7 +102,7 @@ function getLatestChromeDriverVersion() {
 function getReleaseNotes(latestChromeDriverVersion) {
   return new Promise((resolve, reject) => {
     request(
-      'https://chromedriver.storage.googleapis.com/' + 
+      'https://chromedriver.storage.googleapis.com/' +
         latestChromeDriverVersion +
         '/notes.txt',
       (error, response, body) => {
@@ -101,7 +122,7 @@ function findVersionInReleaseNotes(releaseNotes, chromeMajorVersion) {
   // Supports Chrome v69-71
 
   const lines = releaseNotes.split('\n');
-  
+
   for (let i = 0, n = lines.length; i < n; i++) {
     const line = lines[i];
 
@@ -127,7 +148,7 @@ function getChromeDriverVersion() {
     Promise.all(
       [
         getChromeMajorVersion(),
-        getLatestChromeDriverVersion()  
+        getLatestChromeDriverVersion()
       ]
     )
       .then((results) => {
@@ -139,7 +160,7 @@ function getChromeDriverVersion() {
             resolve({
               chromeVersion: chromeVersion.chromeVersion,
               chromeDriverVersion: findVersionInReleaseNotes(
-                releaseNotes, 
+                releaseNotes,
                 chromeVersion.chromeMajorVersion
               )
             });
@@ -152,4 +173,4 @@ module.exports = {
   getChromeDriverVersion: getChromeDriverVersion
 };
 
-// getChromeDriverVersion().then(result => console.log(result));
+getChromeDriverVersion().then(result => console.log(result));
